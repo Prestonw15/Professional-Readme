f7449abde1eba899d5781181dd7d0195a5e82f38
Prestonw15 <prestonwatson.15@gmail.com>
2022-02-25 01:58:15 -0700

    added a lot towards the markdown for readme function making commit before conitinuing

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,19 +1,88 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+//Function to return a message if user decides to have no contributers
+function renderContributing(confirmContributers, data) {
+  if (!confirmContributers) {
+    return `
+    Sorry But I will not be accepting contributers for this project.
+    `;
+  }else {
+    return `
+    ${data}
+    `;
+  }
+}
 
-// TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseBadge(license) {
+  if (license !== 'no license') {
+    return `
+    
+    `;
+  } else {
+    return ' ';
+  }
+}
 
-// TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseLink(license) {
+  if (license !== 'no license') {
+    return `
+    
+    `;
+  } else {
+    return ' ';
+  }
+}
+
+// If there is no license, return an empty string
+function renderLicenseSection(license) {
+  if (license !== 'no license') {
+    return `
+    ## [License](#table-of-contents)
+
+    The application is covered under the following license: 
+    
+    ${renderLicenseLink(license)}
+    `;
+  } else {
+    return ' ';
+  }
+}
+
+//If there is no license, return an empty string
+function renderLicenseTOC(license) {
+  if (license !== 'no license') {
+    return `
+    *[License](#license)
+    `;
+  } else {
+    return ' ';
+  }
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return `
+  # ${data.title}
 
+  ${renderLicenseBadge(data.license)}
+
+  ## Table-of-Contents
+
+  * [Description](#description)
+  * [Installation](#installation)
+  * [Usage](#usage)
+  ${renderLicenseTOC(data.license)}
+  *[Contributing](#contributing)
+  *[Tests](#tests)
+  *[Questions](#questions)
+
+  ## [Description](#table-of-contents)
+
+  ${data.what}
+
+  ${data.why}
+
+  ${data.how}
 `;
 }
 
