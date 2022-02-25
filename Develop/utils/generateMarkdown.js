5106c3a8b685c8dc3a66e1965f3bfe1da4994119
Prestonw15 <prestonwatson.15@gmail.com>
2022-02-25 02:11:29 -0700

    finished the generated Markdown page and moving on to the generated read me now

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -59,7 +59,7 @@ function renderLicenseTOC(license) {
   }
 }
 
-// TODO: Create a function to generate markdown for README
+// function to generate markdown for README
 function generateMarkdown(data) {
   return `
   # ${data.title}
@@ -83,7 +83,35 @@ function generateMarkdown(data) {
   ${data.why}
 
   ${data.how}
+
+## [Installation](#table-of-contents)
+
+${data.installation}
+
+## [Usage](#table-of-contents)
+
+${data.usage}
+
+${renderLicenseSection(data.license)}
+
+## [Contributing](#table-of-contents)
+
+${renderContributing(data.confirmContributers, data.contribute)}
+
+##[Tests](#table-of-contents)
+
+${data.test}
+
+## [Questions](#table-of-contents)
+
+Please contact me using the following links:
+
+[GitHub](https://github.com/${data.githubUsername})
+
+[Email: ${data.email}](mailto:${data.email})
 `;
 }
+
+module.exports = generateMarkdown;
 
 module.exports = generateMarkdown;
