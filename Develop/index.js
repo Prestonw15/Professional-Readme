a5c906695ef4c15c6161f313234842f9086f1220
Prestonw15 <prestonwatson.15@gmail.com>
2022-02-25 00:37:20 -0700

    added a couple of questions to get the title and link to github

--- Develop/index.js
+++ Develop/index.js
@@ -3,10 +3,34 @@ const inquirer = require('inquirer');
 const generateMarkdown = require('./utils/generateMarkdown.js');
 const fs = require('fs');
 // TODO: Create an array of questions for user input
-function inquirer = () => {
-
-}
-
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is the title of your project? (Required)',
+        validate: titleInput => {
+            if (titleInput) {
+                return true;
+            } else {
+                console.log('Please enter your title!');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'githubUsername',
+        message: 'What is your GitHub Username? (Required)',
+        validate: githubInput => {
+            if (githubInput) {
+                return true;
+            } else {
+                console.log('Please enter your GitHub username!');
+                return false;
+            }
+        }
+    },
+];
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
